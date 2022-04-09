a3157afba6debfd00e1b9d024e897485ac2bd7ac
Ismoiljon (Jon) <abduraimovismoil98@gmail.com>
2022-04-10 03:25:14 +0900

    done with Rating button in the Sidebar

--- src/components/Filters/index.jsx
+++ src/components/Filters/index.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
+import Rating from "../../utilities/Rating";
+import "./filter.css";
 
 const Filters = () => {
+  const [rate, setRate] = useState(5);
+
   return (
     <div className="filters">
       <span className="title">Filter Products</span>
@@ -43,7 +47,11 @@ const Filters = () => {
       </span>
       <span>
         <label style={{ paddingRight: 10 }}>Rating</label>
-        {/* <Rating rating={byRating} style={{ cursor: "pointer" }} /> */}
+        <Rating
+          rating={rate}
+          onClick={(i) => setRate(i + 1)}
+          style={{ cursor: "pointer" }}
+        />
       </span>
       <Button variant="light">Clear Filters</Button>
     </div>
